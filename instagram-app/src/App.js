5ed4db1f2124c533cb1609e21d7bc013f5f94077
Kayla <6lackmacaroon@gmail.com>
2019-04-29 15:00:39 -0600

    boilerplate built for comment section

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
+import CommentSection from './components/CommentSection/CommentSection';
 import dummyData from './dummy-data';
-
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBar />
-      <PostContainer />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: []
+    };
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+        <PostContainer />
+        <CommentSection />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './commentSection.css'
+
+const CommentSection = (props) => {
+      return (
+            <div className='comment'>
+                  <h1>user name</h1>
+                  <p>comment</p>
+                  
+            </div>
+      )
+}
+
+
+export default CommentSection;
